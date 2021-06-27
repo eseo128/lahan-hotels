56d8ac321685eb73f317145cf29938f3b0146c16
eseo128 <34049770+eseo128@users.noreply.github.com>
2021-06-28 02:43:58 +0900

    CSS: dining

--- js/main.js
+++ js/main.js
@@ -16,9 +16,20 @@ $(function(){
         fade: true,//페이드효과
         autoplay:true, //자동재생
         autoplaySpeed: 4000, //재생시간
-        pauseOnHover: false,
-        pauseOnFocus: false   
+        pauseOnHover: false,//마우스 호버시 정지
+        pauseOnFocus: false   //포커스시 정지
     });
+
+    //slide2
+    $('.intro_dining .slide2').slick({
+        arrows: false,  //화살표
+        dots: false, //인디케이트 해제
+        fade: false,//페이드효과
+        autoplay:true, //자동재생
+        autoplaySpeed: 4000, //재생시간
+        pauseOnHover: false,//마우스 호버시 정지
+        pauseOnFocus: false   //포커스시 정지
+    })
 });
 
 //탭메뉴
